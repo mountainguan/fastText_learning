c5b9135951b7fc6d2ce62a7656fd67cdc75bd7e7
MG <247744438@qq.com>
2018-04-27 16:53:56 +0800

    add more predict content

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import jieba
 import fastText
 
-input="东莞理工学院"
+# input="石碣第十一中学"
+input = "东莞理工学院"
+# input = "胜和塘贝村村北十二巷1-103号"
 
 text = jieba.cut_for_search(input)
 text = " ".join(text)
@@ -9,4 +11,7 @@ text = " ".join(text)
 classify = fastText.load_model("model/classify.model")
 result = classify.predict(text)
 
+print("预测词：" + input + "\n")
+
+print("预测结果：")
 print(result)
